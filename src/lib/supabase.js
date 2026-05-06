7f009ec1eaf91d2df1920e3382dc806596f6558e
cameronchristensen1 <cameronchristensen1@gmail.com>
2026-05-06 22:09:16 +0000

    Fix Supabase key

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://vxwmjuvahbyqtzlphmad.supabase.co'
-const supabaseKey = 'sb_publishable_7Gl6EWOpcv02y2BH3xr-6w_5dXNB-GU'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ4d21qdXZhaGJ5cXR6bHBobWFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwODQ5NTAsImV4cCI6MjA5MzY2MDk1MH0.eOCNtQ4Sp-OCxESu0c5FAZH6JzA3m22FADei_ToUPC8'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
